18b9f1d9ff8795c14ecdd1903d2aa473f4ff491f
LA208602-Gouthiere <la208602@student.helha.be>
2024-04-03 10:07:31 +0200

    Ajout fonction de saisie de mot dans clavier.c

--- Sources/clavier.c
+++ Sources/clavier.c
@@ -1,16 +1,31 @@
 #include "../Includes/common.h"
 #include "../Includes/clavier.h"
+#include "../Includes/ecran.h"
 
-// Lit au clavier et renvoit une chaîne de caractères en l'affichant
-// Renvoie:
-// - Le pointeur vers de la mémoire allouée avec la chaîne de caractères
-// Note: cette chaîne devra être libérée par l'appelant plus tard
-char *LireTexte()
+/// @brief Lit au clavier et renvoit une chaîne de caractères en l'affichant
+/// @return chaine de caractères
+char * LireTexte()
 {
-   // FONCTION UTILISEE:
-   // Cf LPP2 - ncurses
-   // scanw()
-   
+   char caractere;
+   char * texte = (char *)malloc(1);
+   checkAllocation(texte);
 
-   return NULL; // A adapter
+   // Ajoute les caractères au string tant que ENTER n'est pas appuyé
+   int carPos = 0;
+   while ((caractere = getch()) != '\n') {
+      texte[carPos] = caractere;
+      carPos++;
+      texte = (char *)realloc(texte, carPos + 1); // Rajoute de la mémoire pour 1 caractère supplémentaire
+      checkAllocation(texte);
+   }
+   texte[carPos] = '\0'; // Ajout du null byte
+   
+   return texte;
+}
+
+/// @brief Vérifie si l'allocation mémoire est réussie sinon quitte le programme
+/// @param pointeur Pointeur à vérfier
+void checkAllocation(void * pointeur){
+   if (!pointeur)
+      AfficherErreurEtTerminer("Erreur d'allocation mémoire", -1);
 }
