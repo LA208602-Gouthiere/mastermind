a21d9dd5ea433681b0f4f230a47e5a3872dbd2fd
LA208602-Gouthiere <la208602@student.helha.be>
2024-04-14 15:13:43 +0200

    Assemblage des fonctions dans le main

--- mastermind.c
+++ mastermind.c
@@ -5,21 +5,36 @@
 #include "./Includes/mots.h"
 #include "./Includes/partie.h"
 
-int main()
-{
-    // A CODER - fonctions principale. 
-    // Penser à MM2, LPP2, FBD2
+int main(){
 
-    // FONCTIONS UTILISEES:
-    // InitialiserEcran()
-    // LireDictionnaire()
-    // AfficherErreurEtTerminer()
-    // CreerPartie()
-    // JouerPartie()
+    struct Partie * partie;
+    struct Dictionnaire * dictionnaire;
+    struct Dico_Message * messageDeRetour = (struct Dico_Message *)malloc(sizeof(struct Dico_Message));
+    bool finPartie;
+ 
+    InitialiserEcran();
+    dictionnaire = LireDictionnaire("liste_francais_4.txt", messageDeRetour);
+    // Vérifie si le dictionnaire est bien remplis et lance la partie
+    if(dictionnaire) {
+
+        do {
+            partie = CreerPartie(dictionnaire);
+            finPartie = JouerPartie(partie);
+            EffacerPartie(partie);
+
+        } while(finPartie); // Lance une nouvelle partie si la précédente est terminée et qu'elle n'a pas été abandonnée
+
+        EffacerDictionnaire(dictionnaire);
+        free(messageDeRetour);
+        
+    // Sinon affiche l'erreur et ferme le programme
+    } else {
+        AfficherErreurEtTerminer(messageDeRetour->messageErreur, messageDeRetour->codeErreur);
+    }
+
+    TerminerEcran();
+
     // AfficherMeilleursScores()
-    // EffacerPartie()
-    // EffacerDictionnaire()
-    // TerminerEcran()
     
-    return EXIT_SUCCESS; // A adapter
+    return EXIT_SUCCESS;
 }
